b139bad0de34f1f1dc8df75a2243b191f969ee67
CR0ZFP <gergolorincz100@gmail.com>
2025-04-14 20:56:59 +0200

    Authentication Dto-s created

---
+++ TicketSystemEntitites/Dto/Auth/UserCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Entitites.Dto.Auth
+{
+    public class UserCreateDto
+    {
+        public required string Email { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
+
+        public required string FirstName { get; set; } = string.Empty;
+        public required string LastName { get; set; } = string.Empty;
+    }
+}
