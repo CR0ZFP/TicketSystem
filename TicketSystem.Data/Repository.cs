49441aa98d678f1224b260dd1915a48580a82eef
CR0ZFP <gergolorincz100@gmail.com>
2025-04-14 21:16:45 +0200

    Repository and context created

---
+++ TicketSystem.Data/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.Entitites;
+
+namespace TicketSystem.Data
+{
+    public class Repository<T> where T : class, IIdEntity
+    {
+        TicketSystemContext ctx;
+
+        public Repository(TicketSystemContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+
+        }
+     
+    }
+}
---
+++ TicketSystem.Data/TicketSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicketSystem.Entitites;
+
+namespace TicketSystem.Data
+{
+    public class TicketSystemContext : IdentityDbContext
+    {
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public TicketSystemContext(DbContextOptions<TicketSystemContext> opt): base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
