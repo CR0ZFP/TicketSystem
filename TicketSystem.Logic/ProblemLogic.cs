bd3b33c6ee0d4a0dda0ad81df96674e734f93747
CR0ZFP <gergolorincz100@gmail.com>
2025-04-15 20:25:18 +0200

    Logic created for problems

---
+++ TicketSystem.Logic/ProblemLogic.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TicketSystem.Data;
+using TicketSystem.Entitites.Dto.Problem;
+using TicketSystem.Entitites.Entities;
+using TicketSystem.Logic.Dto;
+
+namespace TicketSystem.Logic
+{
+    public class ProblemLogic
+    {
+        public Repository<Problem> repository;
+        public Mapper Mapper;
+        public ProblemLogic(Repository<Problem> repository, DtoProvider provider)
+        {
+            this.repository = repository;
+            this.Mapper = provider.Mapper;
+        }
+
+        public IEnumerable<ProblemViewDto> Read ()
+        {
+            return repository.GetAll()
+                .Include(x => x.User)
+                .Select(x => Mapper.Map<ProblemViewDto>(x))
+                .ToList();
+        }
+
+        public void Create(ProblemCreateDto problem, string userId)
+        {
+            var entity = Mapper.Map<Problem>(problem);
+            entity.Date = DateTime.Now;
+            entity.UserId = userId;
+            repository.Create(entity);
+        }
+    }
+}
