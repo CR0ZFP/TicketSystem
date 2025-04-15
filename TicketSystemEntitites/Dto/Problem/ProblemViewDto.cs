61df3aaa8094c0f7d2d12216036eb8be08792fb5
CR0ZFP <gergolorincz100@gmail.com>
2025-04-15 20:26:40 +0200

    Problem view dto created

---
+++ TicketSystemEntitites/Dto/Problem/ProblemViewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Entitites.Dto.Problem
+{
+    public class ProblemViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+    }
+}
