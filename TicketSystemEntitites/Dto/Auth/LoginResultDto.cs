f06814eb90257ee68c75119c1fe833d6ac3b11ea
CR0ZFP <gergolorincz100@gmail.com>
2025-04-15 20:26:11 +0200

    Access token dto created

---
+++ TicketSystemEntitites/Dto/Auth/LoginResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Entitites.Dto.Auth
+{
+    public class LoginResultDto
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
