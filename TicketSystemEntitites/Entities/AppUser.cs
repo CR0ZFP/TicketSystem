bc6808fb73fbd9d349a2f7d7e3b2636db4ca2121
CR0ZFP <gergolorincz100@gmail.com>
2025-04-15 20:22:51 +0200

    Fixed entities

---
+++ TicketSystemEntitites/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Entitites.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(200)]
+        public required string GivenName { get; set; } = string.Empty;
+        [StringLength(200)]
+        public required string FamilyName { get; set; } = string.Empty;
+    }
+}
---
+++ TicketSystemEntitites/Entities/Problem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketSystem.Entitites.Entities
+{
+    public class Problem : IIdEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        [MaxLength(450)]
+        public string UserId { get; set; }
+
+        public AppUser User { get; set; }
+        public DateTime Date { get; set; }
+        [StringLength(250)]
+        public string Description { get; set; }
+    }
+}
