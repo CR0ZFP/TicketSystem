74ed065f53180ee2461a68882a6e88ca101bbe24
CR0ZFP <gergolorincz100@gmail.com>
2025-04-15 20:24:45 +0200

    Ensure created fix

--- TicketSystem.Data/TicketSystemContext.cs
+++ TicketSystem.Data/TicketSystemContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using TicketSystem.Entitites;
+using TicketSystem.Entitites.Entities;
 
 namespace TicketSystem.Data
 {
@@ -11,7 +11,7 @@ namespace TicketSystem.Data
 
         public TicketSystemContext(DbContextOptions<TicketSystemContext> opt): base(opt)
         {
-
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
